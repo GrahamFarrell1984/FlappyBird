e174b323a327cd0c47e4c3dd76a170357a2834dd
Graham Farrell <grahamfarrell1984@gmail.com>
2018-10-22 00:02:00 +0100

    Added code for update method in Pipes class.

--- src/game/Pipes.java
+++ src/game/Pipes.java
@@ -40,14 +40,73 @@ public class Pipes implements Updatable, Renderable {
     
     public void resetPipes() {
         
+        currentPipe = 0;
         
+        x1 = Game.WIDTH * 2;
+        x2 = x1 + pipeWidth + pipeHorizontalSpacing;
+        x3 = x2 + pipeWidth + pipeHorizontalSpacing;
+        
+        y1 = getRandomY();
+        y2 = getRandomY();
+        y3 = getRandomY();
+    
+    }
+    
+    private int getRandomY() {
+        
+        return rand.nextInt((int) (Game.HEIGHT * 0.4f)) + (Game.HEIGHT / 10);
         
     }
 
     @Override
     public void update(Input input) {
     
+        x1 = x1 + xVel;
+        x2 = x2 + xVel;
+        x3 = x3 + xVel;
         
+        if(x1 + pipeWidth < 0) {
+            
+            x1 = Game.WIDTH;
+            y1 = getRandomY();
+            currentPipe = 1;
+            
+        }
+        
+        if(x2 + pipeWidth < 0) {
+            
+            x2 = Game.WIDTH;
+            y2 = getRandomY();
+            currentPipe = 2;
+            
+        }
+        
+        if(x3 + pipeWidth < 0) {
+            
+            x3 = Game.WIDTH;
+            y3 = getRandomY();
+            currentPipe = 0;
+            
+        }
+        
+        // Update The Pipe Coordinates
+        
+        switch(currentPipe) {
+            
+            case 0:
+                pipeCoords[0][0] = x1;
+                pipeCoords[0][1] = y1;
+                break;
+            case 1:
+                pipeCoords[1][0] = x2;
+                pipeCoords[1][1] = y2;
+                break;
+            case 2:
+                pipeCoords[2][0] = x3;
+                pipeCoords[2][1] = y3;
+                break;
+            
+        }
     
     }
 
